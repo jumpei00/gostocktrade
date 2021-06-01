d7130788f65cb3533b4256b521e6e0db812aa388
jumpei00 <bs.albireo@gmail.com>
2021-06-01 21:15:51 +0900

    candle indicator api created

--- app/server/web.go
+++ app/server/web.go
@@ -1,35 +1,61 @@
 package server
 
 import (
+	"github.com/jumpei00/gostocktrade/stock"
 	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+
 	"github.com/jumpei00/gostocktrade/app/models"
 	"github.com/jumpei00/gostocktrade/config"
-	"fmt"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
+var temp = template.Must(template.ParseFiles("templates/index.html"))
 
-func candlesAPIHandler(w http.ResponseWriter, req *http.Request) {
-	// TODO
-	df := models.GetCandles(50)
+func indexAPIHandler(w http.ResponseWriter, req *http.Request) {
+	temp.ExecuteTemplate(w, "index.html", nil)
+}
 
-	js, err := json.Marshal(df)
+func candleGetAPIHandler(w http.ResponseWriter, req *http.Request) {
+	models.AllDeleteCandles()
+	stockData, err := stock.GetStockData("VOO", 365)
 	if err != nil {
-		logrus.Warnf("json marshal error: %v", err)
+		logrus.Warnf("stock get error: %v", err)
+	}
+	models.NewCandlesFromQuote(stockData).CreateCandles()
+
+	cframe := models.GetCandles(365)
+
+	js, err := json.Marshal(cframe)
+	if err != nil {
+		logrus.Warnf("candle json error: %v", err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 }
 
-func helloWorld(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello World"))
+func indicatorAPIHandler(w http.ResponseWriter, req *http.Request) {
+	iframe := models.NewIndicator(365)
+	iframe.AddEma(8)
+
+	js, err := json.Marshal(iframe)
+	if err != nil {
+		logrus.Warnf("indicator json error: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 // Run starts webserver
 func Run() {
-	http.HandleFunc("/", helloWorld)
-	http.HandleFunc("/candles", candlesAPIHandler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", indexAPIHandler)
+	http.HandleFunc("/candles", candleGetAPIHandler)
+	http.HandleFunc("/indicator", indicatorAPIHandler)
 	logrus.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil))
 }
--- stock/stock.go
+++ stock/stock.go
@@ -3,8 +3,6 @@ package stock
 import (
 	"time"
 
-	"github.com/sirupsen/logrus"
-
 	"github.com/markcheno/go-quote"
 )
 
@@ -13,15 +11,15 @@ const timeFormat = "2006-01-02"
 // GetStockData dawnloads daily stockdata for symbol(GOOGL, FB...etc) during today ~ before dayPeriod.
 // dayPeriod must be day(1day, 30days...etc).
 // If stock data is not dawnloaded due to bad symbol, output panic.
-func GetStockData(symbol string, dayPeriod int) *quote.Quote {
+func GetStockData(symbol string, dayPeriod int) (*quote.Quote, error) {
 	endDay := time.Now()
 	startDay := endDay.AddDate(0, 0, -dayPeriod)
 
 	stock, err := quote.NewQuoteFromYahoo(
 		symbol, startDay.Format(timeFormat), endDay.Format(timeFormat), quote.Daily, true)
 	if err != nil {
-		logrus.Warnf("stock get error: %v", err)
+		return nil, err
 	}
 
-	return &stock
+	return &stock, nil
 }
