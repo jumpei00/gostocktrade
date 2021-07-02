5cfaa2ece4b8fa3e70c4be189d9aeda971c61f12
jumpei00 <bs.albireo@gmail.com>
2021-07-02 13:35:31 +0900

    javascript mod

--- static/main.js
+++ static/main.js
@@ -1,8 +1,11 @@
 import { viewRealTime, viewChart, viewBacktestResults, viewTrade, viewSignal, removeSignal } from "./view.js"
 import { candleGetRequest, backtestRequest, signalRequest, mappingParams } from "./request.js"
 
-const candle = document.querySelector("#candle")
-const backtest = document.querySelector("#backtest")
+const candle = document.querySelector("#candle");
+const backtest = document.querySelector("#backtest");
+
+// cache getting symbol now
+let now_getting = ""
 
 // getButtonAction is executed when GET button is pushed
 function getButtonAction() {
@@ -19,12 +22,17 @@ function candlesGet() {
     const query = new URLSearchParams({ symbol: symbol, period: period, get: true });
 
     candleGetRequest("/candles", query).then(function (json) {
+        // cached
+        now_getting = symbol;
+
         const result_tag = backtest.querySelector("#results");
         const trade_tag = backtest.querySelector("#trade");
 
         viewChart(symbol, json["candles"]);
         viewBacktestResults(result_tag, json["optimized_params"], signalButtonAction);
         viewTrade(trade_tag, json["trade"]);
+    }).catch(function (e) {
+        alert(e);
     })
 }
 
@@ -38,11 +46,21 @@ function testButtonAction() {
 
 // executeBacktest executes backtest
 function executeBacktest() {
-    const params = backtest.querySelector("#params")
-    let backtest_params = mappingParams(params)
+    const symbol = candle.querySelector("#symbol").value;
+    if (symbol != now_getting) {
+        alert(`different ticker symbol from candle's it.\nbacktest ticker: ${symbol}\ncandle ticker: ${now_getting}`);
+        return
+    }
 
-    backtest_params.symbol = candle.querySelector("#symbol").value
-    backtest_params.period = +backtest.querySelector("#period").value
+    const params = backtest.querySelector("#params")
+    let backtest_params, err, message = mappingParams(params);
+    if (!err) {
+        alert(message);
+        return
+    }
+
+    backtest_params.symbol = symbol;
+    backtest_params.period = +backtest.querySelector("#period").value;
 
     backtestRequest("/backtest", backtest_params).then(function (json) {
         const result_tag = backtest.querySelector("#results");
@@ -50,6 +68,8 @@ function executeBacktest() {
 
         viewBacktestResults(result_tag, json["optimized_params"], signalButtonAction);
         viewTrade(trade_tag, json["trade"]);
+    }).catch(function (e) {
+        alert(e);
     })
 }
 
@@ -65,9 +85,16 @@ function signalButtonAction(signal) {
 // signalGet gets signals(BUY or SELL) for some indicators from server 
 function signalGet(indicator) {
     const symbol = candle.querySelector("#symbol").value;
+    if (symbol != now_getting) {
+        alert(`different ticker symbol from candle's it.\nsignal ticker: ${symbol}\ncandle ticker: ${now_getting}`);
+        return
+    }
+
     const query = new URLSearchParams({ symbol: symbol, [indicator]: true })
     signalRequest("/candles", query).then(function (json) {
         viewSignal(symbol, indicator, json["signals"]);
+    }).catch(function (e) {
+        alert(e);
     })
 }
 
--- static/request.js
+++ static/request.js
@@ -28,46 +28,77 @@ const backtest_params = {
 
 // settings params sending server
 export function mappingParams(params) {
-    backtest_params.ema.short_low = +params.querySelector("#ema_short_low").value
-    backtest_params.ema.short_high = +params.querySelector("#ema_short_high").value
-    backtest_params.ema.long_low = +params.querySelector("#ema_long_low").value
-    backtest_params.ema.long_high = +params.querySelector("#ema_long_high").value
+    let message = ""
 
-    backtest_params.bb.n_low = +params.querySelector("#bb_n_low").value
-    backtest_params.bb.n_high = +params.querySelector("#bb_n_high").value
-    backtest_params.bb.k_low = +params.querySelector("#bb_k_low").value
-    backtest_params.bb.k_high = +params.querySelector("#bb_k_high").value
+    backtest_params.ema.short_low = +params.querySelector("#ema_short_low").value;
+    backtest_params.ema.short_high = +params.querySelector("#ema_short_high").value;
+    backtest_params.ema.long_low = +params.querySelector("#ema_long_low").value;
+    backtest_params.ema.long_high = +params.querySelector("#ema_long_high").value;
+    if (backtest_params.ema.short_low > backtest_params.ema.short_high ||
+        backtest_params.ema.long_low > backtest_params.ema.long_high) {
+        message = "wrong ema parameters, please check magnitude relation(low >= high?)";
+        return backtest_params, false, message
+    }
 
-    backtest_params.macd.fast_low = +params.querySelector("#macd_fast_low").value
-    backtest_params.macd.fast_high = +params.querySelector("#macd_fast_high").value
-    backtest_params.macd.slow_low = +params.querySelector("#macd_slow_low").value
-    backtest_params.macd.slow_high = +params.querySelector("#macd_slow_high").value
-    backtest_params.macd.signal_low = +params.querySelector("#macd_signal_low").value
-    backtest_params.macd.signal_high = +params.querySelector("#macd_signal_high").value
+    backtest_params.bb.n_low = +params.querySelector("#bb_n_low").value;
+    backtest_params.bb.n_high = +params.querySelector("#bb_n_high").value;
+    backtest_params.bb.k_low = +params.querySelector("#bb_k_low").value;
+    backtest_params.bb.k_high = +params.querySelector("#bb_k_high").value;
+    if (backtest_params.bb.n_low > backtest_params.bb.n_high ||
+        backtest_params.bb.k_low > backtest_params.bb.k_high) {
+        message = "wrong bb parameters, please check magnitude relation(low >= high?)";
+        return backtest_params, false, message
+    }
 
-    backtest_params.rsi.period_low = +params.querySelector("#rsi_period_low").value
-    backtest_params.rsi.period_high = +params.querySelector("#rsi_period_high").value
-    backtest_params.rsi.buy_low = +params.querySelector("#rsi_buy_low").value
-    backtest_params.rsi.buy_high = +params.querySelector("#rsi_buy_high").value
-    backtest_params.rsi.sell_low = +params.querySelector("#rsi_sell_low").value
-    backtest_params.rsi.sell_high = +params.querySelector("#rsi_sell_high").value
+    backtest_params.macd.fast_low = +params.querySelector("#macd_fast_low").value;
+    backtest_params.macd.fast_high = +params.querySelector("#macd_fast_high").value;
+    backtest_params.macd.slow_low = +params.querySelector("#macd_slow_low").value;
+    backtest_params.macd.slow_high = +params.querySelector("#macd_slow_high").value;
+    backtest_params.macd.signal_low = +params.querySelector("#macd_signal_low").value;
+    backtest_params.macd.signal_high = +params.querySelector("#macd_signal_high").value;
+    if (backtest_params.macd.fast_low > backtest_params.macd.fast_high ||
+        backtest_params.macd.slow_low > backtest_params.macd.slow_high ||
+        backtest_params.macd.signal_low > backtest_params.macd.signal_high){
+        message = "wrong macd parameters, please check magnitude relation(low >= high?)";
+        return backtest_params, false, message
+    }
 
-    backtest_params.willr.period_low = +params.querySelector("#willr_period_low").value
-    backtest_params.willr.period_high = +params.querySelector("#willr_period_high").value
-    backtest_params.willr.buy_low = -params.querySelector("#willr_buy_low").value
-    backtest_params.willr.buy_high = -params.querySelector("#willr_buy_high").value
-    backtest_params.willr.sell_low = -params.querySelector("#willr_sell_low").value
-    backtest_params.willr.sell_high = -params.querySelector("#willr_sell_high").value
+    backtest_params.rsi.period_low = +params.querySelector("#rsi_period_low").value;
+    backtest_params.rsi.period_high = +params.querySelector("#rsi_period_high").value;
+    backtest_params.rsi.buy_low = +params.querySelector("#rsi_buy_low").value;
+    backtest_params.rsi.buy_high = +params.querySelector("#rsi_buy_high").value;
+    backtest_params.rsi.sell_low = +params.querySelector("#rsi_sell_low").value;
+    backtest_params.rsi.sell_high = +params.querySelector("#rsi_sell_high").value;
+    if (backtest_params.rsi.period_low > backtest_params.rsi.period_high ||
+        backtest_params.rsi.buy_low > backtest_params.rsi.buy_high ||
+        backtest_params.rsi.sell_low > backtest_params.rsi.sell_high) {
+        message = "wrong rsi parameters, please check magnitude relation(low >= high?)";
+        return backtest_params, false, message
+    }
 
-    return backtest_params
+    backtest_params.willr.period_low = +params.querySelector("#willr_period_low").value;
+    backtest_params.willr.period_high = +params.querySelector("#willr_period_high").value;
+    backtest_params.willr.buy_low = -params.querySelector("#willr_buy_low").value;
+    backtest_params.willr.buy_high = -params.querySelector("#willr_buy_high").value;
+    backtest_params.willr.sell_low = -params.querySelector("#willr_sell_low").value;
+    backtest_params.willr.sell_high = -params.querySelector("#willr_sell_high").value;
+    if (backtest_params.willr.period_low > backtest_params.willr.period_high ||
+        backtest_params.willr.buy_low > backtest_params.willr.buy_high ||
+        backtest_params.willr.sell_low > backtest_params.willr.sell_high) {
+        message = "wrong willr parameters, please check magnitude relation(low >= high?)";
+        return backtest_params, false, message
+    }
+
+    return backtest_params, true, message
 }
 
 // candleGetRequest fetches any data from server, return json
 // this method is only used to get candle data 
 export async function candleGetRequest(uri, query) {
-    let response = await fetch(uri + "?" + query)
+    let response = await fetch(uri + "?" + query);
     if (!response.ok) {
-        throw new Error(`HTTP error, starus: ${response.status}`)
+        throw new Error(
+            `HTTP error, status: ${response.status}, message: ${response.json["message"]}`)
     }
     return response.json()
 }
@@ -81,10 +112,11 @@ export async function backtestRequest(uri, params) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(params)
-    })
+    });
 
     if (!response.ok) {
-        throw new Error(`HTTP error, status: ${response.status}`)
+        throw new Error(
+            `HTTP error, status: ${response.status}, message: ${response.json["message"]}`)
     }
 
     return response.json()
@@ -93,9 +125,10 @@ export async function backtestRequest(uri, params) {
 // signalRequest fetches any data from server, return json
 // signalRequest is only used to get signals(BUY or SELL)
 export async function signalRequest(uri, query) {
-    let response = await fetch(uri + "?" + query)
+    let response = await fetch(uri + "?" + query);
     if (!response.ok) {
-        throw new Error(`HTTP error, status: ${response.status}`)
+        throw new Error(
+            `HTTP error, status: ${response.status}, message: ${response.json["message"]}`)
     }
     return response.json()
 }
